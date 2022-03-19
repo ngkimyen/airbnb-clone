064f920cfb934c0547c7b039631135b76b40bf8f
Nguyen Kim Yen <ngkimyen7901@gmail.com>
2022-03-19 19:38:43 +0700

    fix bug

--- src/features/HomePage/FlexibleFindingSection.jsx
+++ src/features/HomePage/FlexibleFindingSection.jsx
@@ -4,13 +4,15 @@ import SearchContext from "../../store/SearchContext";
 import { useContext } from "react";
 
 import "./FlexibleFindingSection.scss";
+import useViewPort from "../../custom-hooks/useViewPort";
 
 const FlexibleFindingSection = () => {
   const [state,] = useContext(SearchContext);
+  const {isSmall} = useViewPort(); 
 
   return (
     <section className="flexible-finding">
-      <div className="container u-padding--large u-padding-bottom-90"  style={{ paddingTop: !state.showSearchBlock && "18rem"}}>
+      <div className="container u-padding--large u-padding-bottom-90" style={{ paddingTop: (!state.showSearchBlock && !isSmall) && "18rem"}}>
         <BannerContainer 
           url={flexibleImage} 
           cssClass="u-flexible-height u-align-bottom u-no-border-radius-big" 
